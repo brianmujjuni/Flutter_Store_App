7a604c1699c59b5e329e2bfcf9a30b1f233e18dd
brianmujjuni <brianmujjuni@engineer.com>
2025-01-14 00:39:35 +0300

    add http depency to manage http requests and also file to manage http

---
+++ lib/services/manage_http_response.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void manageHttpResponse({
+  required http.Response response, //the http response from the request
+  required BuildContext context, //the context is to show snackbar
+  required VoidCallback
+      onSuccess, //the callback to excute on a successfull response
+}) {
+  //switch statement to handle different http status codes
+  switch (response.statusCode) {
+    case 200:
+      onSuccess();
+      break;
+    case 400:
+      showSnackBar(context, json.decode(response.body)['msg']);
+      break;
+    case 500:
+       showSnackBar(context, json.decode(response.body)['error']);
+       break;
+    case 201:
+      onSuccess();
+  }
+}
+
+void showSnackBar(BuildContext context, String title) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(title)));
+}
