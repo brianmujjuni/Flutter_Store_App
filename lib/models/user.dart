bbb664c9ed1a2fca85b07e7208de255356250cbe
brianmujjuni <brianmujjuni@engineer.com>
2025-01-13 22:56:13 +0300

    serailize user object

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -9,5 +9,17 @@ class User {
 
   User({required this.id, required this.fullname, required this.email, required this.state, required this.locality, required this.password});
   
-
+//serialization: Convert user object to a map 
+//A map is a collection of key value pair
+//Convert to a map is an intermediate step that makes it to serialize to the obbject like json format
+Map<String,dynamic> toMap(){
+  return <String, dynamic>{
+    "id": id,
+    "fullname": fullname,
+    "email": email,
+    "state": state,
+    "locality": locality,
+    "password": password
+  };
+}
 }
