8929f629070348e58f5dbb2267f6ca3002bfb04e
brianmujjuni <brianmujjuni@engineer.com>
2025-01-13 22:48:09 +0300

    create User model

---
+++ lib/models/user.dart
@@ -0,0 +1,13 @@
+class User {
+
+  final String id;
+  final String fullname;
+  final String email;
+  final String state;
+  final String locality;
+  final String password;
+
+  User({required this.id, required this.fullname, required this.email, required this.state, required this.locality, required this.password});
+  
+
+}
