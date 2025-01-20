680ad9d09a7d6e5533e00eef6c25ad752065c669
brianmujjuni <brianmujjuni@engineer.com>
2025-01-20 14:43:59 +0300

    complete header ui

--- lib/views/screens/nav_screens/widgets/header_widget.dart
+++ lib/views/screens/nav_screens/widgets/header_widget.dart
@@ -5,7 +5,7 @@ class HeaderWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height * 0.20,
       child: Stack(
@@ -42,6 +42,42 @@ class HeaderWidget extends StatelessWidget {
               ),
             ),
           ),
+          Positioned(
+            left: 311,
+            top: 78,
+            child: Material(
+              type: MaterialType.transparency,
+              child: InkWell(
+
+                onTap: () {},
+                // overlayColor: WidgetStatePropertyAll(Colors.blue.shade500),
+                child: Ink(
+                  width: 31,
+                  height: 31,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/icons/bell.png"),
+                      ),
+                     ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(left: 357, top: 78, child: Material(
+            type: MaterialType.transparency,
+            child: InkWell(
+              onTap: (){},
+              child: Ink(
+                width: 31,
+                height: 31,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/icons/message.png"),
+                  ),
+                ),
+              ),
+            ),
+          ),)
         ],
       ),
     );
