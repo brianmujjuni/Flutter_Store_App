9c870da96cc515b8aa2cc0f145f306339d0ec29a
brianmujjuni <brianmujjuni@engineer.com>
2025-04-08 21:11:23 +0300

    add border radious to banner widget

--- lib/views/screens/nav_screens/widgets/banner_widget.dart
+++ lib/views/screens/nav_screens/widgets/banner_widget.dart
@@ -10,11 +10,15 @@ class BannerWidget extends StatefulWidget {
 class _BannerWidgetState extends State<BannerWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 170,
-      decoration: BoxDecoration(
-        color: Color(0xFFF7F7F7),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 170,
+        decoration: BoxDecoration(
+          color: Color(0xFFF7F7F7),
+          borderRadius: BorderRadius.circular(4),
+        ),
       ),
     );
   }
