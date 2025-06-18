b28eceef1bbec1e5420759b105cd4df5db40169c
brianmujjuni <brianmujjuni@engineer.com>
2025-06-18 22:31:28 +0300

    create add to cart positioned to product item

--- lib/views/screens/nav_screens/widgets/product_item_widget.dart
+++ lib/views/screens/nav_screens/widgets/product_item_widget.dart
@@ -1,5 +1,6 @@
 import 'package:automex_store/models/product.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProductItemWidget extends StatelessWidget {
   // const ProductItemWidget({super.key});
@@ -27,10 +28,52 @@ class ProductItemWidget extends StatelessWidget {
                   height: 170,
                   width: 170,
                   fit: BoxFit.cover,
+                ),
+                Positioned(
+                  top: 15,
+                  right: 10,
+                  child: Image.asset(
+                    'assets/icons/love.png',
+                    width: 26,
+                    height: 26,
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: Image.asset(
+                    'assets/icons/cart.png',
+                    height: 26,
+                    width: 26,
+                  ),
                 )
               ],
             ),
-          )
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Text(
+            product.productName,
+            overflow: TextOverflow.ellipsis,
+            style: GoogleFonts.roboto(
+                fontSize: 13,
+                color: Color(0xFF212121),
+                fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 4,
+          ),
+          Text(
+            product.category,
+            style: GoogleFonts.quicksand(
+              fontSize: 13,
+              fontWeight: FontWeight.bold,
+              color: Color(
+                0xff868D94,
+              ),
+            ),
+          ),
         ],
       ),
     );
