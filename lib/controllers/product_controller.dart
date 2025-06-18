3ca5bf24f331ec944078f1212eb3f36d4cfd8aa3
brianmujjuni <brianmujjuni@engineer.com>
2025-06-18 15:24:01 +0300

    create function to fetch all products

---
+++ lib/controllers/product_controller.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'package:automex_store/global_variables.dart';
+import 'package:automex_store/models/product.dart';
+import 'package:http/http.dart' as http;
+class ProductController {
+
+  Future<List<Product>> fetchCategories() async {
+    try {
+      final response = await http.get(Uri.parse("$uri/api/popular-products"),
+          headers: <String, String>{
+            "Content-Type": "application/json; charset=UTF-8"
+          });
+     
+
+      if (response.statusCode == 200) {
+        final List<dynamic> data = jsonDecode(response.body);
+        List<Product> products =
+            data.map((product) => Product.fromJson(product)).toList();
+        return products;
+      } else {
+        throw Exception("Failed to load products");
+      }
+    } catch (err) {
+      throw Exception("Error loading products: $err");
+    }
+  }
+}
