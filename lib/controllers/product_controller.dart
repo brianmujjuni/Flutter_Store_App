3be56d848a2953c9afca36dabe10c5d3d7538f81
brianmujjuni <brianmujjuni@engineer.com>
2025-06-19 17:42:08 +0300

    create function to fetch products by category

--- lib/controllers/product_controller.dart
+++ lib/controllers/product_controller.dart
@@ -8,19 +8,44 @@ class ProductController {
   Future<List<Product>> fetchProducts() async {
     // use a try block to handle any exception that might occur in the http response
     try {
-      final response = await http.get(Uri.parse("$uri/api/popular-products"),
+      http.Response response = await http.get(
+          Uri.parse("$uri/api/popular-products"),
           //set the http headers
           headers: <String, String>{
             "Content-Type": "application/json; charset=UTF-8"
           });
-        
+
       if (response.statusCode == 200) {
         final List<dynamic> data = jsonDecode(response.body);
-        
+
         List<Product> products = data
             .map((product) => Product.fromMap(product as Map<String, dynamic>))
             .toList();
-        
+
+        return products;
+      } else {
+        throw Exception("Failed to load products");
+      }
+    } catch (err) {
+      throw Exception("Error loading products: $err");
+    }
+  }
+
+  Future<List<Product>> loadProductByCategory(String category) async {
+    try {
+      http.Response response = await http.get(
+          Uri.parse('$uri/api/products-by-category/$category'),
+          headers: <String, String>{
+            "Content-Type": "application/json; charset=UTF-8"
+          });
+
+      if (response.statusCode == 200) {
+        final List<dynamic> data = jsonDecode(response.body);
+
+        List<Product> products = data
+            .map((product) => Product.fromMap(product as Map<String, dynamic>))
+            .toList();
+
         return products;
       } else {
         throw Exception("Failed to load products");
