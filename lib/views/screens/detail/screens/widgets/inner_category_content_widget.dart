8197ebe2f3adfcae3b81f05e51e702604c2b5694
brianmujjuni <brianmujjuni@engineer.com>
2025-06-20 18:22:17 +0300

    create ProductDetails Screen

--- lib/views/screens/detail/screens/widgets/inner_category_content_widget.dart
+++ lib/views/screens/detail/screens/widgets/inner_category_content_widget.dart
@@ -103,35 +103,37 @@ class _InnerCategoryContentWidgetState
             ),
             ReusableTextWidget(title: 'Popular Products', subtitle: 'View all'),
             FutureBuilder(
-        future: futureProducts,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text('Error ${snapshot.error}'),
-            );
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(
-              child: Text('No products under this category'),
-            );
-          } else {
-            final products = snapshot.data;
-            return SizedBox(
-              height: 250,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: products!.length,
-                itemBuilder: (context, index) {
-                  final product = products[index];
-                  return ProductItemWidget(product: product,);
-                },
-              ),
-            );
-          }
-        })
+                future: futureProducts,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text('Error ${snapshot.error}'),
+                    );
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return Center(
+                      child: Text('No products under this category'),
+                    );
+                  } else {
+                    final products = snapshot.data;
+                    return SizedBox(
+                      height: 250,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: products!.length,
+                        itemBuilder: (context, index) {
+                          final product = products[index];
+                          return ProductItemWidget(
+                            product: product,
+                          );
+                        },
+                      ),
+                    );
+                  }
+                }),
           ],
         ),
       ),
