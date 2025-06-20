25449ba1412d6a4156664436d5ab90da73625a54
brianmujjuni <brianmujjuni@engineer.com>
2025-06-20 18:48:35 +0300

    change product screen constructor

--- lib/views/screens/detail/screens/product_detail_screen.dart
+++ lib/views/screens/detail/screens/product_detail_screen.dart
@@ -1,7 +1,11 @@
+import 'package:automex_store/models/product.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProductDetailScreen extends StatefulWidget {
-  const ProductDetailScreen({super.key});
+ final Product product;
+
+  const ProductDetailScreen({super.key, required this.product});
 
   @override
   State<ProductDetailScreen> createState() => _ProductDetailScreenState();
@@ -10,6 +14,16 @@ class ProductDetailScreen extends StatefulWidget {
 class _ProductDetailScreenState extends State<ProductDetailScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Product Detail",
+          style: GoogleFonts.quicksand(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
   }
 }
