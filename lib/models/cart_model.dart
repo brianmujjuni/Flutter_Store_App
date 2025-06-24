5c4729adcbc3a74919609748dfe71a3c23ce12da
brianmujjuni <brianmujjuni@engineer.com>
2025-06-24 21:47:12 +0300

    create function to increment quantity

--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -1,14 +1,24 @@
-class Cart{
+class Cart {
   final String productName;
   final int productPrice;
   final String category;
-  final List image;
+  final List<String> image;
   final String vendorId;
   final int productQuantity;
-  final int quantity;
+  int quantity;
   final String productId;
   final String description;
   final String fullName;
 
-  Cart({required this.productName, required this.productPrice, required this.category, required this.image, required this.vendorId, required this.productQuantity, required this.quantity, required this.productId, required this.description, required this.fullName});
+  Cart(
+      {required this.productName,
+      required this.productPrice,
+      required this.category,
+      required this.image,
+      required this.vendorId,
+      required this.productQuantity,
+      required this.quantity,
+      required this.productId,
+      required this.description,
+      required this.fullName});
 }
---
+++ lib/provider/cart_provider.dart
@@ -0,0 +1,65 @@
+//notifier class to manage the cart state,extending stateNotifier with an initial state of empty map
+import 'package:automex_store/models/cart_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CartNotifier extends StateNotifier<Map<String, Cart>> {
+  CartNotifier() : super({});
+
+  //Method to add product to the cart
+  void addProductToCart({
+    required String productName,
+    required int productPrice,
+    required String category,
+    required List<String> image,
+    required String vendorId,
+    required int productQuantity,
+    required int quantity,
+    required String productId,
+    required String description,
+    required String fullName,
+  }) {
+    //check if the product is already in the cart
+    if (state.containsKey(productId)) {
+      //if the product is already in the cart,update it's quantity and maybe other details
+      state = {
+        ...state,
+        productId: Cart(
+          productName: state[productId]!.productName,
+          productPrice: state[productId]!.productPrice,
+          category: state[productId]!.category,
+          image: state[productId]!.image,
+          vendorId: state[productId]!.vendorId,
+          productQuantity: state[productId]!.productQuantity,
+          quantity: state[productId]!.quantity + 1,
+          productId: state[productId]!.productId,
+          description: state[productId]!.description,
+          fullName: state[productId]!.fullName,
+        )
+      };
+    } else {
+      //if the product not in cart add it with the provided details
+      state = {
+        productId: Cart(
+          productName: productName,
+          productPrice: productPrice,
+          category: category,
+          image: image,
+          vendorId: vendorId,
+          productQuantity: productQuantity,
+          quantity: quantity,
+          productId: productId,
+          description: description,
+          fullName: fullName,
+        )
+      };
+    }
+  }
+  //method to increment the quantity of a product in the cart
+  void IncrementCartItem(String productId){
+    if(state.containsKey(productId)){
+      state[productId]!.quantity++;
+      //Notify listeners that the state has change
+      state ={...state};
+    }
+  }
+}
